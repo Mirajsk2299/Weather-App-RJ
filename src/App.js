d9df5cb9c760b5c0dd3836849bdd5fe804bac8ea
Miraj Shaikh <98844021+Mirajsk2299@users.noreply.github.com>
2025-11-29 23:20:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import "./App.css";
 
 function App() {
   const [weatherInfo, setWeatherInfo] = useState(null);
-  const [cityName, setCityName] = useState("Pune"); // default city
+  const [cityName, setCityName] = useState("Pune"); 
 
-  // const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
           `https://api.weatherapi.com/v1/forecast.json?key=f82b355f317b4eef930160425250910&q=${cityName}&days=5&aqi=no&alerts=no`
-          // `https://api.weatherapi.com/v1/current.json?key=f82b355f317b4eef930160425250910&q=${cityName}&aqi=no`
         );
         setWeatherInfo(response.data);
       } catch (error) {
@@ -24,19 +22,7 @@ function App() {
     fetchData();
   }, [cityName]);
 
-  // //-----------------  5 days data     -------------------
-  // useEffect(() => {
-  //   const secondData = async () => {
-  //     try {
-  //       const response =
-  //         await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=f82b355f317b4eef930160425250910&q=Pune&days=5&aqi=no&alerts=no`);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
 
-  //   secondData();
-  // }, []);
 
   return (
     <div
